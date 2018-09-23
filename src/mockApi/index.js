08630482dab9d8b11951be4542148c1d3a3900cc
dianekrauthamer <diane.krauthamer@gmail.com>
2018-09-23 14:05:12 -0400

    Update index.js

--- src/mockApi/index.js
+++ src/mockApi/index.js
@@ -14,15 +14,18 @@ const get = id => new Promise((resolve, reject) => {
 })
 
 /**
- * Mock POST method. Returns whatever is passed after a delay of 250ms. For mocking purposes, has a 20% chance of throwing an error.
+ * Displays UI states when completing an update request, and when the update is complete
+ 
  * @param {any} data 
  */
 const post = data => new Promise((resolve, reject) => {
   if(Math.random() < .2) {
     console.log('Whoops – API error.')
+    return ('Whoops – API error.')
     setTimeout(() => reject(data), 250)
   } else {
     console.log('Successful request!')
+    return ('Successful request!')
     setTimeout(() => resolve(data), 250)
   }
 })
